b51249188278522af0a4c91b2d9c0687815bc6ae
HChlebek <student@hchlebek.local>
2016-05-12 11:33:10 -0500

    myRecipes alertView and tableview

--- MealManager1/MyRecipesVC.swift
+++ MealManager1/MyRecipesVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MyRecipesVC: UIViewController
+class MyRecipesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
     @IBOutlet var myRecipesTableView: UITableView!
     var recipes : [Recipe] = []
@@ -17,6 +17,8 @@ class MyRecipesVC: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        myRecipesTableView.dataSource = self
+        myRecipesTableView.delegate = self
         
     }
     
@@ -30,10 +32,25 @@ class MyRecipesVC: UIViewController
             let myRecipeTextField = myAlert.textFields! [0] as UITextField
             self.recipes.append(Recipe(Name: myRecipeTextField.text!, Image: UIImage(named: "default")!))
             self.myRecipesTableView.reloadData()
-            
+    
+    }
+        myAlert.addAction(addAction)
+        myAlert.addTextFieldWithConfigurationHandler { (myRecipeTextField) -> Void in
+            myRecipeTextField.placeholder = "Add a recipe name"
+        }
+    self.presentViewController(myAlert, animated: true, completion: nil)
     }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let myTableViewCell = myRecipesTableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
+        myTableViewCell.textLabel?.text = recipes[indexPath.row].name
+        
+        return myTableViewCell
+    }
     
-    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return recipes.count
     }
 }
